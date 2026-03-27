75fb2f29c815594efc8bb5d161492f741749b507
Gurumurthyshivani <shivanigurumurthy10@gmail.com>
2026-03-27 11:07:39 +0530

    use gemini 2.0-flash

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ App.use(express.json())
 
 
 let genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
-let model = genAI.getGenerativeModel({model:"gemini-1.5-flash"})
+let model = genAI.getGenerativeModel({model:"gemini-2.0-flash"})
 
 App.post('/ask' , 
     async (req,res)=>{
